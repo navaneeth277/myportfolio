be3173044d8f1eb9a38ca226b7f982a3e3a1306d
navaneeth277 <anavaneeth277@gmail.com>
2025-02-12 16:41:40 +0530

    addad note

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,6 +11,7 @@ const Projects = ({ isDarkMode }) => {
       title: "Expre - Exam Preparation Assistant",
       description: "Expre is a web app built with React, Node.js, and the Gemini API to help with exam preparation. It includes features like summarizing content, extracting keywords, writing essays, and coding in languages like C++, Java, Python, and C. It’s designed with sections for questions, materials, and coding to make studying easier for students.",
       image: Im3,// Use public folder images directly
+      note:" NOTE : Let me know when you want to see it working—I need to start the backend.",
       github: "https://github.com/Navaneeth2707/",
       demo: "https://expre-frountend-taupe.vercel.app/",
     },
@@ -18,6 +19,7 @@ const Projects = ({ isDarkMode }) => {
       title: "To Do List",
       description: "Your to-do list app is a simple and user-friendly tool built using React. It allows users to add, edit, and delete tasks, helping them stay organized and manage their daily activities efficiently.",
       image: Im2,
+      note:"",
       github: "https://github.com/Navaneeth2707/To_Do_List",
       demo: "https://github.com/Navaneeth2707/To_Do_List",
     },
@@ -25,6 +27,7 @@ const Projects = ({ isDarkMode }) => {
       title: "Find date using date",
       description: "The Find Date feature calculates a future or past date based on a given date. It simplifies date-based planning by allowing users to add or subtract days from the selected date easily.",
       image: Im1,
+      note:"",
       github: "https://github.com/Navaneeth2707/Day-of-Date",
       demo: "https://github.com/Navaneeth2707/Day-of-Date",
     },
@@ -45,6 +48,8 @@ const Projects = ({ isDarkMode }) => {
             <div className="project-card-content">
               <h3>{project.title}</h3>
               <p>{project.description}</p>
+              <p>{project.note}</p>
+
               <div className="project-links">
                 <a
                   href={project.github}
